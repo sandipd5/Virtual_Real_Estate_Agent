6382eea4d2e8229f697cd5f75931cd709698b774
Jyotsna Sehgal <jyotsnasehgal24@gmail.com>
2024-08-09 12:52:12 +0000

    changes in structure of project

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import joblib
 import numpy as np
 import pandas as pd
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from extract_entities import extract_entities
-from filter_properties import filter_properties
-from answer_composition import RealEstateChatbot
+from Utils.extract_entities import extract_entities
+from Utils.filter_properties import filter_properties
+from Utils.llama_response import fine_tuned_resp
+from Utils.answer_composition import RealEstateChatbot
 
 
 app = Flask(__name__)
@@ -66,19 +67,21 @@ def predict():
     new_sequence = tokenizer.texts_to_sequences([user_input])
     new_padded_sequence = pad_sequences(new_sequence, maxlen=max_sequence_length, padding='post')
 
-    # Predict the intent
+    #Predict the intent
     prediction = model.predict(new_padded_sequence)
     predicted_intent = np.argmax(prediction)
     
     # Decode the predicted intent
     predicted_intent_array = np.array([predicted_intent])
     predicted_intent_label = le.inverse_transform(predicted_intent_array)[0]
-
-    # Extract entities from the user input
-    entities = extract_entities(user_input)
+    print(predicted_intent_label,'predicted_intent_label')
+    response=fine_tuned_resp(user_input)
+    
+    # # Extract entities from the user input
+    # entities = extract_entities(user_input)
  
-    #Generate a response based on the predicted intent and extracted entities
-    response = chatbot.compose_answer(predicted_intent_label, entities, property_df)
+    # #Generate a response based on the predicted intent and extracted entities
+    # response = chatbot.compose_answer(predicted_intent_label, entities, property_df)
 
     # Return the prediction to the user
     #return render_template('index.html', prediction=predicted_intent_label,response = response)
